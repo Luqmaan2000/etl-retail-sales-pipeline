91edc2a2506a080df98c669646a286a9808e8359
Luqmaan <Luqmaan2000@hotmail.co.uk>
2025-05-01 18:48:20 +0100

    Extract and load dataset: added CSV download and load with encoding fix

--- ETL Pipeline.py
+++ ETL Pipeline.py
@@ -1,1 +1,10 @@
-print("Hello")
+import pandas as pd
+import os
+
+# Load the correct CSV path
+
+csv_path = os.path.join("superstore-dataset-final", "Sample - Superstore.csv")
+df = pd.read_csv(csv_path, encoding='latin1')
+
+print("✅ CSV Loaded Successfully:")
+print(df.head())
