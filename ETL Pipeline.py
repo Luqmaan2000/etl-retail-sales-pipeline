688d9d7eaf3d6a639142e6e6cdebc83458f999f3
Luqmaan <Luqmaan2000@hotmail.co.uk>
2025-05-02 18:28:58 +0100

    Created SQLite database and queried data

--- ETL Pipeline.py
+++ ETL Pipeline.py
@@ -10,13 +10,10 @@ csv_path = os.path.join("superstore-dataset-final", "Sample - Superstore.csv")
 df = pd.read_csv(csv_path, encoding='latin1')
 
 print("✅ CSV Loaded Successfully:")
-df.head(2)
 
-
-# Transform Data
+# ------------ Transform Data -----------------
 # %%
 df.columns = [col.strip().lower().replace(" ","_") for col in df.columns]
-df.head(2)
 
 # %%
 # Removing null values
@@ -28,6 +25,12 @@ else:
 
 
 # %%
+# Changing profits and sales to 2 decimal places
+
+df['profit'] = df['profit'].round(2)
+df['sales'] = df['sales'].round(2)
+
+# %%
 # Convert Dates + Engineer New Column
 
 df['order_date'] = pd.to_datetime(df['order_date'])
@@ -37,6 +40,52 @@ df['ship_date'] = pd.to_datetime(df['ship_date'])
 
 df['delivery_days'] = (df['ship_date']-df['order_date'])
 
-df[['order_date','ship_date','delivery_days']].head(2)
 
+# %%
+# Optimizing Data Types (Categorical Columns)
+
+cat_cols = ['segment','country','region','city','sub-category','ship_mode','product_name']
+for col in cat_cols:
+    df[col] = df[col].astype('category')
+
+print('Converted categorical columns into categories')
+
+# %%
+# Summary Stats
+# Sales & Profit by Category
+category_summary = df.groupby('category')[['sales','profit']].sum().sort_values(by='sales',ascending = False)
+print("Total Sales and Profit by Category")
+print(category_summary)
+
+# Average Delivery Time By Region
+
+delivery_by_region = df.groupby('region')['delivery_days'].mean().sort_values()
+print('\n Average Time of Deliveries by Region')
+print(delivery_by_region)
+
+# Top 5 Sub Categories by Total Sales
+
+top_subcats = df.groupby('sub-category')['sales'].sum().sort_values(ascending = False).head(5)
+print('\n Top Sub Categories by Sales:')
+print(top_subcats)
+
+# ----------- Load Phase ------------------
+# %%
+import sqlite3
+
+conn = sqlite3.connect("sales_data.db")
+
+df.to_sql("sales",conn, index = False)
+
+
+# %%
+
+query = """
+SELECT category, SUM(sales) as total_sales
+From sales  
+Group by category  
+order by total_sales Desc
+"""
+result = pd.read_sql(query,conn)
+print(result)
 # %%
