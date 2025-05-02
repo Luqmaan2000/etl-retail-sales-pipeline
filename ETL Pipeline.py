608a88b48339d54f5fb4c9e6f06200a7c419fd51
Luqmaan <Luqmaan2000@hotmail.co.uk>
2025-05-02 15:43:15 +0100

    Transformed Data: changed column names to lower case, checked for null values, converted dates and created new column for delivery days

--- ETL Pipeline.py
+++ ETL Pipeline.py
@@ -1,5 +1,8 @@
+# %%
 import pandas as pd
 import os
+
+
 
 # Load the correct CSV path
 
@@ -7,4 +10,33 @@ csv_path = os.path.join("superstore-dataset-final", "Sample - Superstore.csv")
 df = pd.read_csv(csv_path, encoding='latin1')
 
 print("✅ CSV Loaded Successfully:")
-print(df.head())
+df.head(2)
+
+
+# Transform Data
+# %%
+df.columns = [col.strip().lower().replace(" ","_") for col in df.columns]
+df.head(2)
+
+# %%
+# Removing null values
+
+if df.isnull().values.any():
+    raise ValueError("Dataset contains error values")
+else:
+    print("No missing values detected")
+
+
+# %%
+# Convert Dates + Engineer New Column
+
+df['order_date'] = pd.to_datetime(df['order_date'])
+df['ship_date'] = pd.to_datetime(df['ship_date'])
+
+## Delivery duration
+
+df['delivery_days'] = (df['ship_date']-df['order_date'])
+
+df[['order_date','ship_date','delivery_days']].head(2)
+
+# %%
